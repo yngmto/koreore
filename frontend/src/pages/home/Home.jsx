3dfba296be67bb0953784a5781fb9ae36affaa4e
yngmto <kawaiidog415@gmail.com>
2024-09-19 03:55:07 +0900

    HOMEjsx APIURL修正

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,11 +5,12 @@ import Timeline from '../../components/timeline/Timeline';
 import axios from 'axios';
 
 export default function Home() {
-  const API_URL = process.env.REACT_APP_API_URL;
+  const API_URL = process.env.API_URL;
   console.log("API_URL",API_URL);
 
   const testConnection = async () => {
     try {
+
       const response = await axios.get(`${API_URL}/api/test`);
       console.log(response.data.message);
     } catch (error) {
