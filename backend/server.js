6b40ef44a3dcfbf89ccb42c57558c6a1c31fa6c4
yngmto <139634671+yngmto@users.noreply.github.com>
2024-09-18 16:21:58 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,10 +37,12 @@ app.use((err, req, res, next) => {
 // クライアントのビルドフォルダを静的ファイルとして提供
 //app.use(express.static(path.join(__dirname, '../client/build')));
 
+//※Vercelでは、静的ファイルは
+//.vercel/output/static ディレクトリに配置する必要がある。
 // その他のルートはReactアプリにリダイレクト
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+//app.get('*', (req, res) => {
+//  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+//});
 
 // Vercelでのデプロイ時には不要？
 app.listen(process.env.PORT || 5000, () => {
